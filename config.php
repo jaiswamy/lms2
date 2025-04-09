7fffe62595892a1851ac30c1d444b48f1bb2b7bb
jaichandraswamy <jaichandraswamy@gmail.com>
2025-04-09 03:28:02 -0700

    update

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'mariadb';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'localhost';
+$CFG->dbname    = 'u499940289_lmsmoodle';
+$CFG->dbuser    = 'u499940289_LmSMooDlEs';
+$CFG->dbpass    = 'LmsMooDlES@2024';
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => '',
+  'dbsocket' => '',
+  'dbcollation' => 'utf8mb4_unicode_ci',
+);
+
+$CFG->wwwroot   = 'https://lms.hiideals.com';
+$CFG->dataroot  = '/home/u499940289/domains/hiideals.com/public_html/moodledata';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 0777;
+
+require_once(__DIR__ . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
